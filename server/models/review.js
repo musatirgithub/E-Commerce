5128b22e3b6fb3cafb527aad837497328e062d58
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-03 22:13:22 +0100

    feat: Review model setup

---
+++ server/models/review.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = mongoose.Schema({
+    title:{
+        type:String,
+        minLength:[3, 'Title should be at least 3 characters.'],
+        maxLength:[100, 'Title cannot be longer than 100 characters.'],
+    },
+    comment:{
+        type:String,
+        minLength:[3, 'Comment should be at least 3 characters.'],
+        maxLength:[1000, 'Comment cannot be longer than 1000 characters.'],
+    },
+    rating:{
+        type:Number,
+        min:1,
+        max:5,
+        required:[true, 'Please provide product rating'],
+        default:5,
+    },
+    product:{
+        type:mongoose.Types.ObjectId,
+        ref:'Product',
+        required:true,
+    },
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+}, {timeStamps:true})
