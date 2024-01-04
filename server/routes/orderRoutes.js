c5734e0de58f7d631bd2cded3e3084e1d806f79c
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-04 23:35:45 +0100

    feat: OrderRoutes setup

---
+++ server/routes/orderRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
+const {getAllOrders, getUserOrders, createOrder, deleteOrder, updateOrder, getOrder} = require('../controllers/taskController');
+
+router.get('/all-orders', authenticateUser, authorizePermissions('admin'), getAllOrders);
+router.get('/', authenticateUser, getUserOrders);
+router.post('/', authenticateUser, createOrder);
+router.delete('/:id', authenticateUser, deleteOrder);
+router.patch('/:id', authenticateUser, updateOrder);
+router.get('/:id', authenticateUser, getOrder);
+
+module.exports = router;
