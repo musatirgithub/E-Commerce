e325fde8105100e5085168b8412b250a83d95718
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-04 23:36:20 +0100

    feat: reviewRoutes setup

---
+++ server/routes/reviewRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
+const {getAllReviews, getUserReviews, createReview, deleteReview, updateReview, getReview} = require('../controllers/taskController');
+
+router.get('/all-reviews', authenticateUser, authorizePermissions('admin'), getAllReviews);
+router.get('/', authenticateUser, getUserReviews);
+router.post('/', authenticateUser, createReview);
+router.delete('/:id', authenticateUser, deleteReview);
+router.patch('/:id', authenticateUser, updateReview);
+router.get('/:id', authenticateUser, getReview);
+
+module.exports = router;
