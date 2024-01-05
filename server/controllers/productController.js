700686ec24209b4d7e9d206e8c62c2ef3bbb375e
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-05 22:43:37 +0100

    feat: GetProducts filter, search, paging add

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,34 +4,88 @@ const {StatusCodes} = require('http-status-codes');
 const checkPermissions = require('../utils/checkPermissions');
 
 const getProducts = async (req, res)=>{
-    const products = await Product.find({user:req.user.userId}).select('-user').sort('')
-    res.status(StatusCodes.OK).json({products})
+    const {featured, company, name, sort, fields, numericFilters} = req.query;
+
+    const queryObject = {};
+
+    if (featured){
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (company){
+        queryObject.company = company;
+    }
+    if (name){
+        queryObject.name = {$regex:name, $options:'i'};
+    }
+    if(numericFilters){
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            'e':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+        }
+        const regEx = /\b(<|<=|=|>|>=)\b/g
+        let filter = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
+        const options = ['price', 'rating'];
+        filter.split(',').forEach((item)=>{
+            const [field , operator, value] = item.split('-');
+            if(options.includes(field)){
+            const [field , operator, value] = item.split('-');
+                queryObject[field] = {[operator]:Number(value)}
+            }
+        }) 
+        };
+    
+
+    let result = Product.find(queryObject);
+
+    if (sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }else{
+        result = result.sort('createdAt')
+    }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = page - 1 * limit;
+    result = result.skip(skip).limit(limit);
+
+    console.log(result);
+    const products = await result;
+    res.status(StatusCodes.OK).json({products, nbItems:products.length})
 }
 const createProduct = async (req, res)=>{
-    const {name, price, description, images, category, company, colors, featured, freeShipping, inventory} = req.body;
-    if(!name || !price || !description || !category || !company || !colors || !inventory){
-        throw new CustomError.BadRequestError('Please provide required data!')
-    }
-    const product = await Product.create({name, price, description, images, category, company, colors, featured, freeShipping, inventory, user:req.user.userId})
+    req.body.user = req.user.userId;
+    // const {name, price, description, images, category, company, colors, featured, freeShipping, inventory} = req.body;
+    // if(!name || !price || !description || !category || !company || !colors || !inventory){
+    //     throw new CustomError.BadRequestError('Please provide all required data!')
+    // }
+    const product = await Product.create(req.body)
     res.status(StatusCodes.CREATED).json({msg:'Success! Product created'})
 }
 const deleteProduct = async (req, res)=>{
     const {id:productId} = req.params;
-    const product = await Product.findOneAndDelete({_id:productId});
+    const product = await Product.findOne({_id:productId});
     if(!product){
         throw new CustomError.NotFoundError(`No product with id: ${productId}`)
     }
-    checkPermissions(req.user, product.user)
+    await product.remove();
     res.status(StatusCodes.OK).json({msg:'Product deleted successfully'})
 }
 const updateProduct = async (req, res)=>{
     const {id:productId} = req.params;
-    const product = await Product.findOneAndUpdate({_id:productId}, req.body, {new:true, runValidators:true})
+    const product = await Product.findOne({_id:productId}, req.body, {new:true, runValidators:true})
     if(!product){
         throw new CustomError.NotFoundError(`No product with ID: ${productId}`);
     }
-
-    checkPermissions(req.user, product.user);
+    await product.save();
     res.status(StatusCodes.OK).json({msg:'Success! Product updated.'});
 }
 const getProduct = async (req, res)=>{
@@ -40,8 +94,7 @@ const getProduct = async (req, res)=>{
     if(!product){
         throw new CustomError.NotFoundError(`No product with ID: ${productId}`);
     }
-    checkPermissions(req.user, product.user)
-    res.status(StatusCodes.OK).json({task})
+    res.status(StatusCodes.OK).json({product})
 }
 
 module.exports = {getProducts, createProduct, deleteProduct, updateProduct, getProduct};
