8dcfea08fc05bf3bcbb3c50b626cf6be0eb33227
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-06 23:05:13 +0100

    fix: TaskList component name fix

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,17 +1,17 @@
 import { useSelector } from "react-redux";
 import {useEffect, useState} from "react";
-import useTaskCalls from "../hooks/useTaskCalls";
+import useTaskCalls from "../hooks/useProductCalls";
 import { FaPencilAlt } from "react-icons/fa";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { IoWarning } from "react-icons/io5";
 import { useDispatch } from "react-redux";
-import { openModal } from "../features/taskSlice";
+import { openModal } from "../features/productSlice";
 import dateTimeConverter from "../utils/dateTimeConverter";
 import remainingDays from "../utils/remainingDays";
 
 const TaskList = () => {
     const dispatch = useDispatch();
-    const {tasks} = useSelector((state)=>state.task);
+    const {products} = useSelector((state)=>state.products);
 
     const [loading, setLoading] = useState(true)
     const {getTasks, deleteTask, getTask} = useTaskCalls();
@@ -31,8 +31,8 @@ const TaskList = () => {
             <div>Loading...</div>
         )
     }
-    if(tasks && tasks.length === 0){
-        return <section className="text-center pt-5 text-[#EEEDE8]">No tasks to show...</section>
+    if(products && products.length === 0){
+        return <section className="text-center pt-5 text-[#EEEDE8]">No products to show...</section>
     }
   return (
     
@@ -49,19 +49,19 @@ const TaskList = () => {
         </tr>
         </thead>
        <tbody> 
-        {tasks?.map((task, index)=>{
-            const remainder = remainingDays(task.deadline)
+        {products?.map((product, index)=>{
+            const remainder = remainingDays(product.deadline)
             return(
-                <tr key={index} className={`${task.completed ? "lg:w-[64rem] even:bg-[#252E46] line-through":"lg:w-[64rem] even:bg-[#252E46]"}`}>
+                <tr key={index} className={`${product.completed ? "lg:w-[64rem] even:bg-[#252E46] line-through":"lg:w-[64rem] even:bg-[#252E46]"}`}>
                     <td className="flex items-center w-[14rem] lg:w-[35rem]">
                         <div className="w-[1.5rem]">
                     {remainder ? <IoWarning color={remainder} className="w-[1.1rem]"/> : <div className="w-[1.1rem]"></div>}
                     </div>
-                    <h4 className="w-[33.5rem] line-clamp-2 lg:line-clamp-1">{task.name}</h4>
+                    <h4 className="w-[33.5rem] line-clamp-2 lg:line-clamp-1">{product.name}</h4>
                     </td>
-                    <td className="w-[3rem] lg:w-[19rem] text-center">{dateTimeConverter(new Date(task.deadline))}</td>
-                    <td className="w-[2.5rem] lg:w-[5rem]"><FaPencilAlt onClick={()=>handleClick(task._id)} className="cursor-pointer w-[1.5rem] mx-auto text-blue-700"/></td>
-                    <td className="w-[2.5rem] lg:w-[5rem]"><FaRegTrashCan onClick={()=>deleteTask(task._id)} className="cursor-pointer w-[1.5rem] mx-auto text-red-700"/></td>
+                    <td className="w-[3rem] lg:w-[19rem] text-center">{dateTimeConverter(new Date(product.deadline))}</td>
+                    <td className="w-[2.5rem] lg:w-[5rem]"><FaPencilAlt onClick={()=>handleClick(product._id)} className="cursor-pointer w-[1.5rem] mx-auto text-blue-700"/></td>
+                    <td className="w-[2.5rem] lg:w-[5rem]"><FaRegTrashCan onClick={()=>deleteTask(product._id)} className="cursor-pointer w-[1.5rem] mx-auto text-red-700"/></td>
                 </tr>
             )
         })}
