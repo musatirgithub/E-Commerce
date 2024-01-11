a91b02120274cf06a7c8a98c7cf3debf707d7160
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-11 23:18:35 +0100

    fix: Table headers update

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -42,24 +42,23 @@ const TaskList = () => {
         <table>
             <thead>
         <tr className="lg:w-[64rem]">
-            <th className="w-[14rem] lg:w-[35rem] text-center">Task</th>
-            <th className="w-[3rem] lg:w-[19rem] text-center">Dline</th>
+            <th className="w-[14rem] lg:w-[35rem] text-center">Name</th>
+            <th className="w-[3rem] lg:w-[19rem] text-center">Price</th>
             <th className="w-[2.5rem] lg:w-[5rem] text-center">Edit</th>
             <th className="w-[2.5rem] lg:w-[5rem] text-center">Del</th>
         </tr>
         </thead>
        <tbody> 
         {products?.map((product, index)=>{
-            const remainder = remainingDays(product.deadline)
             return(
-                <tr key={index} className={`${product.completed ? "lg:w-[64rem] even:bg-[#252E46] line-through":"lg:w-[64rem] even:bg-[#252E46]"}`}>
+                <tr key={index} className={"lg:w-[64rem] even:bg-[#252E46]"}>
                     <td className="flex items-center w-[14rem] lg:w-[35rem]">
                         <div className="w-[1.5rem]">
-                    {remainder ? <IoWarning color={remainder} className="w-[1.1rem]"/> : <div className="w-[1.1rem]"></div>}
-                    </div>
+                            <div className="w-[1.1rem]"></div>
+                        </div>
                     <h4 className="w-[33.5rem] line-clamp-2 lg:line-clamp-1">{product.name}</h4>
                     </td>
-                    <td className="w-[3rem] lg:w-[19rem] text-center">{dateTimeConverter(new Date(product.deadline))}</td>
+                    <td className="w-[3rem] lg:w-[19rem] text-center">{product.price}</td>
                     <td className="w-[2.5rem] lg:w-[5rem]"><FaPencilAlt onClick={()=>handleClick(product._id)} className="cursor-pointer w-[1.5rem] mx-auto text-blue-700"/></td>
                     <td className="w-[2.5rem] lg:w-[5rem]"><FaRegTrashCan onClick={()=>deleteProduct(product._id)} className="cursor-pointer w-[1.5rem] mx-auto text-red-700"/></td>
                 </tr>
