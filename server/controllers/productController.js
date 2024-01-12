02840f50140c27d51c590429b3c3520d7c670c31
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-12 22:36:42 +0100

    fix: UpdateProduct controller fix

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,60 +4,61 @@ const {StatusCodes} = require('http-status-codes');
 const checkPermissions = require('../utils/checkPermissions');
 
 const getProducts = async (req, res)=>{
-    const {featured, company, name, sort, fields, numericFilters} = req.query;
+    // const {featured, company, name, sort, fields, numericFilters} = req.query;
 
-    const queryObject = {};
+    // const queryObject = {};
 
-    if (featured){
-        queryObject.featured = featured === 'true' ? true : false;
-    }
-    if (company){
-        queryObject.company = company;
-    }
-    if (name){
-        queryObject.name = {$regex:name, $options:'i'};
-    }
-    if(numericFilters){
-        const operatorMap = {
-            '>':'$gt',
-            '>=':'$gte',
-            'e':'$eq',
-            '<':'$lt',
-            '<=':'$lte',
-        }
-        const regEx = /\b(<|<=|=|>|>=)\b/g
-        let filter = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
-        const options = ['price', 'rating'];
-        filter.split(',').forEach((item)=>{
-            const [field , operator, value] = item.split('-');
-            if(options.includes(field)){
-            const [field , operator, value] = item.split('-');
-                queryObject[field] = {[operator]:Number(value)}
-            }
-        }) 
-        };
+    // if (featured){
+    //     queryObject.featured = featured === 'true' ? true : false;
+    // }
+    // if (company){
+    //     queryObject.company = company;
+    // }
+    // if (name){
+    //     queryObject.name = {$regex:name, $options:'i'};
+    // }
+    // if(numericFilters){
+    //     const operatorMap = {
+    //         '>':'$gt',
+    //         '>=':'$gte',
+    //         'e':'$eq',
+    //         '<':'$lt',
+    //         '<=':'$lte',
+    //     }
+    //     const regEx = /\b(<|<=|=|>|>=)\b/g
+    //     let filter = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
+    //     const options = ['price', 'rating'];
+    //     filter.split(',').forEach((item)=>{
+    //         const [field , operator, value] = item.split('-');
+    //         if(options.includes(field)){
+    //         const [field , operator, value] = item.split('-');
+    //             queryObject[field] = {[operator]:Number(value)}
+    //         }
+    //     }) 
+    //     };
     
 
-    let result = Product.find(queryObject);
+    // let result = Product.find(queryObject);
 
-    if (sort){
-        const sortList = sort.split(',').join(' ');
-        result = result.sort(sortList);
-    }else{
-        result = result.sort('createdAt')
-    }
+    // if (sort){
+    //     const sortList = sort.split(',').join(' ');
+    //     result = result.sort(sortList);
+    // }else{
+    //     result = result.sort('createdAt')
+    // }
 
-    if(fields){
-        const fieldsList = fields.split(',').join(' ');
-        result = result.select(fieldsList);
-    }
+    // if(fields){
+    //     const fieldsList = fields.split(',').join(' ');
+    //     result = result.select(fieldsList);
+    // }
 
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-    result = result.skip(skip).limit(limit);
+    // const page = Number(req.query.page) || 1;
+    // const limit = Number(req.query.limit) || 10;
+    // const skip = (page - 1) * limit;
+    // result = result.skip(skip).limit(limit);
 
-    const products = await result;
+    // const products = await result;
+    const products = await Product.find({});
     res.status(StatusCodes.OK).json({products, nbItems:products.length})
 }
 const createProduct = async (req, res)=>{
@@ -80,11 +81,10 @@ const deleteProduct = async (req, res)=>{
 }
 const updateProduct = async (req, res)=>{
     const {id:productId} = req.params;
-    const product = await Product.findOne({_id:productId}, req.body, {new:true, runValidators:true})
+    const product = await Product.findOneAndUpdate({_id:productId}, req.body, {new:true, runValidators:true})
     if(!product){
         throw new CustomError.NotFoundError(`No product with ID: ${productId}`);
     }
-    await product.save();
     res.status(StatusCodes.OK).json({msg:'Success! Product updated.'});
 }
 const getProduct = async (req, res)=>{
