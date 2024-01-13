b33dddc8fa13b84504632c8f363ea51fb2ac4a38
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-13 23:57:51 +0100

    feat> UseOrderCalls basic setup

---
+++ client/src/hooks/useOrderCalls.js
@@ -0,0 +1,98 @@
+// import { axiosWithToken } from "../service/axiosInstance";
+import { useDispatch } from "react-redux";
+
+import {
+  fetchStart,
+  getProductsSuccess,
+  getSingleProductSuccess,
+  fetchFail,
+} from "../features/productSlice";
+import {axiosPublic} from "../utils/axiosPublic";
+import { useNavigate } from "react-router-dom";
+import { toastSuccessNotify, toastErrorNotify } from "../helper/ToastNotify";
+
+const useOrderCalls = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // const getAllProducts = async () => {
+  //   dispatch(fetchStart());
+  //   try {
+  //     const { data } = await axiosPublic.get("/api/v1/task/all-tasks", {withCredentials:'include'});
+  //     dispatch(getProductsSuccess(data.products));
+  //   } catch (err) {
+  //     dispatch(fetchFail());
+  //   }
+  // };
+  const getOrders = async () => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axiosPublic.get("/api/v1/order/", {withCredentials:'include'});
+      dispatch(getProductsSuccess(data.orders));
+    } catch (err) {
+      dispatch(fetchFail());
+      // if(err?.response?.status === 401){
+      //   dispatch(tokenTimeout());
+      //   toastErrorNotify("Login required!")
+      //     navigate('/login')
+      // }
+    }
+  };
+  const createOrder = async (taskInfo) => {
+    dispatch(fetchStart());
+    try {
+      const {data} = await axiosPublic.post(`/api/v1/order/`, taskInfo, {withCredentials:'include'});
+      toastSuccessNotify(data.msg)
+      // await getOrders();
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify(err.response.data.msg);
+    }
+  };
+  const deleteOrder = async (id) => {
+    dispatch(fetchStart());
+    try {
+      const {data} = await axiosPublic.delete(`/api/v1/order/${id}`, {withCredentials:'include'});
+      toastSuccessNotify(data.msg)
+      // await getOrders();
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify(err.response.data.msg);
+    }
+  };
+  const getOrder = async (id) => {
+    dispatch(fetchStart());
+    try {
+      const {data} = await axiosPublic.get(`/api/v1/order/${id}`, {withCredentials:'include'});
+      dispatch(getSingleProductSuccess(data.product));
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify(err.response.data.msg);
+    }
+  };
+  const updateOrder = async (orderInfo, id) => {
+    dispatch(fetchStart());
+    try {
+      const {data} = await axiosPublic.patch(`/api/v1/order/${id}`, orderInfo, {withCredentials:'include'});
+      getSingleProductSuccess(data.order);
+      toastSuccessNotify(data.msg);
+      await getOrders();
+      await getOrder(id);
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify(err.response.data.msg);
+    }
+  };
+
+
+  return {
+    // getAllTasks,
+    getOrders,
+    createOrder,
+    deleteOrder,
+    getOrder,
+    updateOrder,
+  };
+};
+
+export default useOrderCalls;
