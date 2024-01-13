208783d64616ac0db779867e07dc2164e4368f11
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-14 00:01:07 +0100

    feat> OrderSlice basic setup

---
+++ client/src/features/orderSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const orderSlice = createSlice({
+  name: "order",
+
+  initialState: {
+    loading: true,
+    error: false,
+    orders: null,
+    order: null,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    // getSuccess: (state, { payload: { data, url } }) => {
+    //   state.loading = false;
+    //   state[url] = data;
+    // },
+
+    getOrdersSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.orders = payload;
+    },
+    getSingleOrderSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.order = payload;
+    },
+
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const {
+  fetchStart,
+  getOrdersSuccess,
+  getSingleOrderSuccess,
+  fetchFail,
+} = orderSlice.actions;
+export default orderSlice.reducer;
