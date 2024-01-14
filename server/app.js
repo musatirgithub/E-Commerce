a48bf5b7ba1ee93dc209ad0b50dda246dcd6a779
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-14 22:23:54 +0100

    feat: Order, Review routers add

--- server/app.js
+++ server/app.js
@@ -27,6 +27,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
+const orderRouter = require('./routes/orderRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 
 // middleware requires
@@ -62,6 +64,8 @@ app.use(fileUpload());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/product', productRouter);
+app.use('/api/v1/order', orderRouter);
+app.use('/api/v1/review', reviewRouter);
 
 
 // notFound & Error uses
