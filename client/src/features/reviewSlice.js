bae00fe4d8fb0e1b6ca640caa973ea38eabfe3c3
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-14 22:12:04 +0100

    feat: ReviewSlice setup

---
+++ client/src/features/reviewSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reviewSlice = createSlice({
+  name: "review",
+
+  initialState: {
+    loading: true,
+    error: false,
+    reviews: null,
+    userReviews:null,
+    review: null,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    // getSuccess: (state, { payload: { data, url } }) => {
+    //   state.loading = false;
+    //   state[url] = data;
+    // },
+
+    getReviewsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.reviews = payload;
+    },
+    getUserReviewsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.userReviews = payload;
+    },
+    getSingleReviewSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.review = payload;
+    },
+
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const {
+  fetchStart,
+  getReviewsSuccess,
+  getUserReviewsSuccess,
+  getSingleReviewSuccess,
+  fetchFail,
+} = reviewSlice.actions;
+export default reviewSlice.reducer;
