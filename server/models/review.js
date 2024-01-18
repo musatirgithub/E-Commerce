104527f55960feb05a3ba49ca355d0cdd96c8822
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-18 22:59:13 +0100

    feat: Review model calculateAverageRating agg func add

--- server/models/review.js
+++ server/models/review.js
@@ -30,8 +30,26 @@ const ReviewSchema = mongoose.Schema({
     },
 }, {timeStamps:true})
 
-ReviewSchema.statics.calculateAverageRating = async function (productId) {
+    ReviewSchema.statics.calculateAverageRating = async function (productId) {
 
+        const result = await this.aggregate([
+            {$match:{product:productId}},
+            {$group:{
+                _id:null,
+                averageRating:{$avg:'§rating'},
+                numOfReviews:{$sum:1},
+            }}
+        ]);
+
+        try {
+            const {data} = await this.model('Product').findOneAndUpdate(
+                {_id:productId},
+                {averageRating:Math.ceil(result[0]?.averageRating || 0),
+                numOfReviews: result[0]?.numOfReviews || 0},
+            )
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     ReviewSchema.post('save', async function(){
