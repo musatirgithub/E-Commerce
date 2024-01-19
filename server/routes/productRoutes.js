4286ef4cafe40fb177dc13f22018dd8b4267fd5f
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-19 22:58:07 +0100

    feat: Upload-image route add

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
-const {getProducts, createProduct, deleteProduct, updateProduct, getProduct} = require('../controllers/productController');
+const {getProducts, createProduct, deleteProduct, updateProduct, getProduct, uploadProductImage} = require('../controllers/productController');
 
 // router.get('/all-products', authenticateUser, authorizePermissions('admin'), getAllProducts);
 router.get('/', getProducts);
 router.post('/', authenticateUser, authorizePermissions('admin'), createProduct);
+router.post('/upload-image', authenticateUser, authorizePermissions('admin'), uploadProductImage);
 router.delete('/:id', authenticateUser, authorizePermissions('admin'), deleteProduct);
 router.patch('/:id', authenticateUser, authorizePermissions('admin'), updateProduct);
 router.get('/:id', getProduct);
