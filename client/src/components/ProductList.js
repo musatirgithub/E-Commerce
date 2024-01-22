517f2fd40264abbb68095a0e3b076b7dd30b4b9b
Mehmet Ugur Satir <musatir@gmail.com>
2024-01-22 20:45:40 +0100

    style: style change from table to card

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux";
 import {useEffect, useState} from "react";
 import useProductCalls from "../hooks/useProductCalls";
-import { FaPencilAlt } from "react-icons/fa";
-import { FaRegTrashCan } from "react-icons/fa6";
-import { IoWarning } from "react-icons/io5";
+// import { FaPencilAlt } from "react-icons/fa";
+// import { FaRegTrashCan } from "react-icons/fa6";
+// import { IoWarning } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { openModal } from "../features/productSlice";
 
 const TaskList = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const {products} = useSelector((state)=>state.product);
 
     const [loading, setLoading] = useState(true)
@@ -22,7 +24,8 @@ const TaskList = () => {
 
     const handleClick = (id)=>{
         getProduct(id);
-        dispatch(openModal());
+        navigate(`/single-product/${id}`);
+        // dispatch(openModal());
     }
     if(loading){
         return(
@@ -34,37 +37,24 @@ const TaskList = () => {
     }
   return (
     
-    <div className="1 text-[#EEEDE8]">
-        {/* <h3 className="text-center text-2xl">Tasks</h3> */}
-        <section className="flex justify-center items-center">
-        <table>
-            <thead>
-        <tr className="lg:w-[64rem]">
-            <th className="w-[14rem] lg:w-[35rem] text-center">Name</th>
-            <th className="w-[3rem] lg:w-[19rem] text-center">Price</th>
-            <th className="w-[2.5rem] lg:w-[5rem] text-center">Edit</th>
-            <th className="w-[2.5rem] lg:w-[5rem] text-center">Del</th>
-        </tr>
-        </thead>
-       <tbody> 
+    <div className="flex flex-wrap gap-5">
+
         {products?.map((product, index)=>{
             return(
-                <tr key={index} className={"lg:w-[64rem] even:bg-[#252E46]"}>
-                    <td className="flex items-center w-[14rem] lg:w-[35rem]">
-                        <div className="w-[1.5rem]">
-                            <div className="w-[1.1rem]"></div>
-                        </div>
-                    <h4 className="w-[33.5rem] line-clamp-2 lg:line-clamp-1">{product.name}</h4>
-                    </td>
-                    <td className="w-[3rem] lg:w-[19rem] text-center">{product.price}</td>
-                    <td className="w-[2.5rem] lg:w-[5rem]"><FaPencilAlt onClick={()=>handleClick(product._id)} className="cursor-pointer w-[1.5rem] mx-auto text-blue-700"/></td>
-                    <td className="w-[2.5rem] lg:w-[5rem]"><FaRegTrashCan onClick={()=>deleteProduct(product._id)} className="cursor-pointer w-[1.5rem] mx-auto text-red-700"/></td>
-                </tr>
+                <div key={index} className={""} onClick={()=>handleClick(product._id)}>
+                    <div className="w-[21rem] h-[21rem] object-cover object-center rounded-t-lg ">
+                        <img src={product.image} className=""/>
+                    </div>
+                    <div className="text-white rounded-b-lg">
+                       <div className="flex justify-between w-[21rem] ">
+                            <p>{product.name}</p>
+                            <p>{`$${product.price/100}`}</p>
+                       </div>
+                       <p className="line-clamp-3">{product.description}</p>
+                    </div>
+                </div>
             )
         })}
-        </tbody>
-        </table>
-        </section>
     </div>
     
   )
