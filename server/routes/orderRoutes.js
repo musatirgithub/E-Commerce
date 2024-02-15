c1fb4561581c2f48b660a85598992c76e9bf43b0
Mehmet Ugur Satir <musatir@gmail.com>
2024-02-15 23:15:01 +0100

    feat: Temporary route add for testing purpose

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
-const {getAllOrders, getUserOrders, createOrder, deleteOrder, updateOrder, getOrder, config} = require('../controllers/orderController');
+const {getAllOrders, getUserOrders, createOrder, deleteOrder, updateOrder, getOrder, config, createPaymentIntent} = require('../controllers/orderController');
 
 router.get('/all-orders', authenticateUser, authorizePermissions('admin'), getAllOrders);
 router.get('/config', authenticateUser, config);
+router.post('/create-payment-intent', authenticateUser, createPaymentIntent);
 router.get('/', authenticateUser, getUserOrders);
 router.post('/', authenticateUser, createOrder);
 router.delete('/:id', authenticateUser, deleteOrder);
