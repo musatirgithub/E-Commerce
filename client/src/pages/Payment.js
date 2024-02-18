7e5ff360258979946951ff3d999442fbb9edd8c3
Mehmet Ugur Satir <musatir@gmail.com>
2024-02-18 22:52:42 +0100

    fix: Temp paymentIntent functionality remove

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -4,30 +4,21 @@ import { loadStripe } from "@stripe/stripe-js";
 import { axiosPublic } from "../utils/axiosPublic";
 import CheckoutForm from "../components/CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useOrderCalls from "../hooks/useOrderCalls";
 
 const Payment = () => {
-  const { cartTotal, cartItems, shipping, tax, orderTotal, address } = useSelector(
+  const { cartTotal, cartItems, shipping, tax, orderTotal, address, clientSecret } = useSelector(
     (state) => state.order
   );
+  const {createOrder} = useOrderCalls();
   const [stripePromise, setStripePromise] = useState(null);
-  const [clientSecret, setClientSecret] = useState(null);
 
-  const orderInfo = {};
+  const orderInfo = {subtotal:cartTotal, cartItems, shipping, tax, total:orderTotal, address};
   const getPK = async () => {
     const { data } = await axiosPublic.get("/api/v1/order/config", {
       withCredentials: "include",
     });
     setStripePromise(loadStripe(data.publishableKey));
-  };
-  const createPaymentIntent = async () => {
-    const { data } = await axiosPublic.post(
-      `/api/v1/order/create-payment-intent`,
-      orderInfo,
-      {
-        withCredentials: "include",
-      }
-    );
-    setClientSecret(data.clientSecret);
   };
 
   useEffect(() => {
@@ -35,16 +26,24 @@ const Payment = () => {
   }, []);
 
   useEffect(() => {
-    createPaymentIntent();
+    createOrder(orderInfo);
   }, []);
   return (
-    <div>
+    <main className="">
+
+    <div className="min-h-[calc(100vh-8rem)] flex justify-center items-center">
       {stripePromise && clientSecret && (
+              <div>
+              <h4 className="text-center font-semibold">Following Payment System is Connected to a Fake Account. You will not be charged anyway!</h4>
+              <h4 className="text-center font-semibold">For Testing Purposes Please Use Following Card Number</h4>
+              <h4 className="text-center text-red-800 font-bold p-4 border border-solid border-orange-800 my-2">4242 4242 4242 4242</h4>
         <Elements stripe={stripePromise} options={{clientSecret}}>
           <CheckoutForm />
         </Elements>
+        </div>
       )}
     </div>
+    </main>
   );
 };
 
