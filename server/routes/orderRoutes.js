1440dfd4c4d5a9377c04bce83e20154be77a49eb
Mehmet Ugur Satir <musatir@gmail.com>
2024-03-18 23:32:35 +0100

    feat: CheckIfUserOrderedProduct controller route

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
-const {getAllOrders, getUserOrders, createOrder, deleteOrder, updateOrder, getOrder, config, createPaymentIntent} = require('../controllers/orderController');
+const {getAllOrders, getUserOrders, createOrder, deleteOrder, updateOrder, getOrder, config, createPaymentIntent, checkIfUserOrderedProduct} = require('../controllers/orderController');
 
 router.get('/all-orders', authenticateUser, authorizePermissions('admin'), getAllOrders);
 router.get('/config', authenticateUser, config);
 router.post('/create-payment-intent', authenticateUser, createPaymentIntent);
 router.get('/', authenticateUser, getUserOrders);
 router.post('/', authenticateUser, createOrder);
+router.get('/check-if-ordered/:id', authenticateUser, checkIfUserOrderedProduct);
 router.delete('/:id', authenticateUser, deleteOrder);
 router.patch('/:id', authenticateUser, updateOrder);
 router.get('/:id', authenticateUser, getOrder);
