9291f85598e6dc7b19c41a4ace2e00a8c0fdbea6
Mehmet Ugur Satir <musatir@gmail.com>
2024-03-21 22:44:24 +0100

    feat: Rating select option add to write comment component

--- client/src/components/WriteComment.js
+++ client/src/components/WriteComment.js
@@ -1,8 +1,39 @@
+import { useState } from "react";
 
 
 const WriteComment = () => {
+  const [commentData, setCommentData] = useState({rating:5, });
+  const handleChange = (e)=>{
+    setCommentData({...commentData, [e.target.name]:e.target.value})
+  }
   return (
-    <div>WriteComment</div>
+    <section>
+    <form>
+    <select
+            name="rating"
+            id="rating"
+            value={commentData.rating}
+            onChange={handleChange}
+            className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-[10.5rem] appearance-none placeholder-[#3A3B3C]"
+          >
+                <option value="5">
+                  5
+                </option>
+                <option value="4">
+                  4
+                </option>
+                <option value="3">
+                  3
+                </option>
+                <option value="2">
+                  2
+                </option>
+                <option value="1">
+                  1
+                </option>
+          </select>
+    </form>
+    </section>
   )
 }
 
