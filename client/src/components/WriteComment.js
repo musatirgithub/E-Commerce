e9a3b98ef352e1142177bace76b8900bd6c58058
Mehmet Ugur Satir <musatir@gmail.com>
2024-03-21 22:58:28 +0100

    feat: Title & comment inputs add

--- client/src/components/WriteComment.js
+++ client/src/components/WriteComment.js
@@ -1,40 +1,56 @@
 import { useState } from "react";
 
-
 const WriteComment = () => {
-  const [commentData, setCommentData] = useState({rating:5, });
-  const handleChange = (e)=>{
-    setCommentData({...commentData, [e.target.name]:e.target.value})
-  }
+  const [commentData, setCommentData] = useState({ rating: 5 });
+  const handleChange = (e) => {
+    setCommentData({ ...commentData, [e.target.name]: e.target.value });
+  };
   return (
     <section>
-    <form>
-    <select
-            name="rating"
-            id="rating"
-            value={commentData.rating}
+      <form>
+        <select
+          name="rating"
+          id="rating"
+          value={commentData.rating}
+          onChange={handleChange}
+          className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-[10.5rem] appearance-none placeholder-[#3A3B3C]"
+        >
+          <option value="5">5</option>
+          <option value="4">4</option>
+          <option value="3">3</option>
+          <option value="2">2</option>
+          <option value="1">1</option>
+        </select>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="title" className="text-[#EEEDE8]">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={commentData.title}
             onChange={handleChange}
-            className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-[10.5rem] appearance-none placeholder-[#3A3B3C]"
-          >
-                <option value="5">
-                  5
-                </option>
-                <option value="4">
-                  4
-                </option>
-                <option value="3">
-                  3
-                </option>
-                <option value="2">
-                  2
-                </option>
-                <option value="1">
-                  1
-                </option>
-          </select>
-    </form>
+            placeholder="Enter comment title..."
+            className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-[21rem] appearance-none placeholder-[#3A3B3C]"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="comment" className="text-[#EEEDE8]">
+            Title
+          </label>
+          <textarea
+            name="comment"
+            id="comment"
+            value={commentData.comment}
+            onChange={handleChange}
+            placeholder="Enter comment here..."
+            className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-[21rem] appearance-none placeholder-[#3A3B3C]"
+          />
+        </div>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default WriteComment
+export default WriteComment;
