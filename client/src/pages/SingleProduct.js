80320e6532648bc422d492558c41c9c3fa111e0f
Mehmet Ugur Satir <musatir@gmail.com>
2024-04-02 00:41:48 +0200

    fix: Loading state receive from global state

--- client/src/pages/SingleProduct.js
+++ client/src/pages/SingleProduct.js
@@ -12,15 +12,15 @@ import useReviewCalls from "../hooks/useReviewCalls";
 const SingleProduct = () => {
     const dispatch = useDispatch();
     const {getProduct} = useProductCalls();
-    const {getReview} = useReviewCalls();
+    const {getReview, getProductReviews} = useReviewCalls();
     // isProductOrdered function checks whether user ordered this product or not
     const {isProductOrdered} = useOrderCalls();
-    const {product} = useSelector((state)=>state.product);
+    const {product, loading} = useSelector((state)=>state.product);
     const {isOrderedByUser} = useSelector((state)=>state.order);
-    const {review} = useSelector((state)=>state.review);
+    const {review, reviews} = useSelector((state)=>state.review);
     const navigate = useNavigate();
     const {id} = useParams();
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
     const [amount, setAmount] = useState(1);
     const [isCommentOpen, setIsCommentOpen] = useState(false);
 
@@ -49,11 +49,12 @@ const SingleProduct = () => {
       }
 
     useEffect(() => {
-        setLoading(true);
+        // setLoading(true);
         getProduct(id);
         isProductOrdered(id);
+        getProductReviews(id);
         getReview(id);
-        setLoading(false);
+        // setLoading(false);
     }, [isOrderedByUser]);
     if(loading){
         return <div>
@@ -62,7 +63,7 @@ const SingleProduct = () => {
     }
   return (
     <main>
-        <div className=" bg-amber-700 rounded-lg text-white hover:bg-amber-600 w-[15rem] uppercase text-center p-2 font-bold my-5 mx-auto" onClick={()=>navigate('/')}>Back to Products</div>
+        <div className=" bg-amber-700 rounded-lg text-white hover:bg-amber-600 w-[15rem] uppercase text-center p-2 font-bold my-5 mx-auto cursor-pointer" onClick={()=>navigate('/')}>Back to Products</div>
         <section>
             <div className="w-[15rem] object-cover object-center mx-auto">
                 <img src={product?.image} alt={product?.name}/>
@@ -86,8 +87,11 @@ const SingleProduct = () => {
             </div>
             <p className=" text-md font-semibold lowercase text-center py-3">({product?.numOfReviews} customer reviews)</p>
             </div>
+            <div>
+            {reviews.length>0 && <p className=" text-md font-semibold text-blue-800 py-3 ps-3 cursor-pointer" onClick={()=>navigate(`/single-product-reviews/${id}`)}>See customer reviews...</p>}
+            </div>
             {isOrderedByUser && <button className="btn btn-warning " onClick={()=>setIsCommentOpen(!isCommentOpen)}>{review?"Update review":"Write review"}</button>}
-            {isCommentOpen && <WriteComment id={id}/>}
+            {isCommentOpen && <WriteComment id={id} setIsCommentOpen={setIsCommentOpen}/>}
             <div className="underline w-[15rem] mx-auto"></div>
             <div className="my-[2rem] flex justify-center gap-3">
                 <div className="min-w-[2rem] bg-gray-300 text-black text-center text-3xl font-bold rounded-md" onClick={handleDecrease}>-</div>
@@ -95,7 +99,7 @@ const SingleProduct = () => {
                 <div className="min-w-[2rem] bg-gray-300 text-black text-center text-3xl font-bold rounded-md " onClick={handleIncrease}>+</div>
             </div>
         </section>
-        <div className=" bg-amber-700 rounded-lg text-white hover:bg-amber-600 w-[15rem] uppercase text-center p-2 font-bold my-5 mx-auto" onClick={handleAddProduct}>Add to Cart</div>
+        <div className=" bg-amber-700 rounded-lg text-white hover:bg-amber-600 w-[15rem] uppercase text-center p-2 font-bold my-5 mx-auto cursor-pointer" onClick={handleAddProduct}>Add to Cart</div>
     </main>
   )
 }
