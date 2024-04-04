d29adee27d25baea0fe8bf0b30dff7ce2ec55e80
Mehmet Ugur Satir <musatir@gmail.com>
2024-04-04 23:01:29 +0200

    feat: Review & product info add

--- client/src/pages/SingleProductReviews.js
+++ client/src/pages/SingleProductReviews.js
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const SingleProductReviews = () => {
-    useEffect(()=>{
-
-    },[])
+  const {reviews} = useSelector((state)=>state.review);
+  const {product} = useSelector((state)=>state.product);
   return (
-    <div>SingleProductReviews</div>
+    <main className="min-h-[calc(100vh-8rem)]">
+      <p className="text-3xl font-bold capitalize text-center py-3">{product.name}</p>
+            <div className="w-[10rem] object-cover object-center mx-auto">
+                <img src={product?.image} alt={product?.name}/>
+            </div>
+            {reviews.map((review)=>{
+              return<div className="border">
+                <p>Rating: {review.rating}</p>
+                <p>Title: {review.title}</p>
+                <p>Review: {review.comment}</p>
+              </div>
+            })}
+    </main>
   )
 }
 
