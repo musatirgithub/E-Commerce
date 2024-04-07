bf01d9450c16067c0fdfdeec8d3cf19ed882f6de
Mehmet Ugur Satir <musatir@gmail.com>
2024-04-07 19:50:13 +0200

    feat: Product review route add

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
-const {getAllReviews, getUserReviews, createReview, deleteReview, updateReview, getReview} = require('../controllers/reviewController');
+const {getAllReviews, getUserReviews, createReview, deleteReview, updateReview, getReview, getProductReviews} = require('../controllers/reviewController');
 
 router.get('/all-reviews', authenticateUser, authorizePermissions('admin'), getAllReviews);
+router.get('/product-reviews/:id', authenticateUser, getProductReviews);
 router.get('/', authenticateUser, getUserReviews);
 router.post('/', authenticateUser, createReview);
 router.delete('/:id', authenticateUser, deleteReview);
