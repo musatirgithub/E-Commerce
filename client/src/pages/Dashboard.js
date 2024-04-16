a778a73f5911bd33ef456d7eaf1d4549d2f4aa66
Mehmet Ugur Satir <musatir@gmail.com>
2024-04-16 23:52:34 +0200

    feat: Create product page move to Dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <main className="min-h-[calc(100vh-8rem)]">
+      <div>
+        <Link to="/create-product">Create New Product</Link>
+      </div>
+      <div>Product List</div>
+      <div>Order List</div>
+    </main>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
