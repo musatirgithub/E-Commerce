fb37d7ddff22a2763510b7d2a73019e18d917f8c
Mehmet Ugur Satir <musatir@gmail.com>
2024-07-13 16:27:41 +0200

    style: Dashboard page style (basic)

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,14 +2,18 @@ import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <main className="min-h-[calc(100vh-8rem)]">
-      <div>
-        <Link to="/create-product">Create New Product</Link>
+    <main className="min-h-[calc(100vh-8rem)] flex justify-center items-center gap-2 lg:gap-8 flex-wrap">
+      {/* <div className=""> */}
+      <div className=" bg-red-800 text-white rounded-md p-2 w-[7rem] lg:w-[20rem] text-center">
+        <Link to="/create-product">Create Product</Link>
       </div>
-      <div>
+      <div className=" bg-red-800 text-white rounded-md p-2 w-[7rem] lg:w-[20rem] text-center">
       <Link to="/productlist-admin">Product List</Link>
       </div>
-      <div>Order List</div>
+      <div className=" bg-red-800 text-white rounded-md p-2 w-[7rem] lg:w-[20rem] text-center">
+        <Link to="/order-list">Order List</Link>
+      </div>
+      {/* </div> */}
     </main>
   );
 };
