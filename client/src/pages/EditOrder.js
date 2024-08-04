2439cc3d99e15666f2432a76ccb2c3fa0d62f366
Mehmet Ugur Satir <musatir@gmail.com>
2024-08-04 17:32:34 +0200

    style: EditOrder page style

--- client/src/pages/EditOrder.js
+++ client/src/pages/EditOrder.js
@@ -26,7 +26,7 @@ const EditOrder = () => {
     updateOrder(orderInfo, order?._id);
   };
   const handleChange = (e) => {
-    setOrderInfo({ ...orderInfo, [e.target.name]: e.target.value});
+    setOrderInfo({ ...orderInfo, [e.target.name]: e.target.value });
   };
   if (loading) {
     return (
@@ -36,62 +36,75 @@ const EditOrder = () => {
     );
   }
   return (
-    <main>
-        <h2 className="text-center text-[1.5rem] font-bold">Order Details</h2>
-      <div className="flex p-2 w-[30rem] mx-auto">
-        <p className="w-[10rem]">Order Created At: </p>
-        <p className="px-2">
-          : {formatDate(order?.createdAt)}
-        </p>
-      </div>
-      <div className="flex p-2 w-[30rem] mx-auto">
-        <p className="w-[10rem]">Order Updated At </p>
-        <p className="px-2">
-          : {formatDate(order?.updatedAt)}
-        </p>
-      </div>
-      <div className="flex p-2 w-[30rem] mx-auto">
-        <p className="w-[10rem]">Order Id </p>
-        <p className="px-2">: {order?._id}</p>
-      </div>
-      <div className="flex p-2 w-[30rem] mx-auto">
-        <p className="w-[10rem]">Order Total </p>
-        <p className="px-2">: {formatPrice(order?.total)}</p>
-      </div>
-      <div className="flex p-2 w-[30rem] mx-auto">
-        <p className="w-[10rem]">Address </p>
-        <p className="px-2">: {order?.address}</p>
-      </div>
-
-      <div className="flex flex-col gap-3 w-[15rem]">
-        <p>Order Items: </p>
-        {order?.orderItems?.map((item, index) => {
-          return (
-            <div key={index}>
-              <p className="w-[14.5rem]">{item.name}</p>
-              <p className="w-[14.5rem]">{formatPrice(item.price)}</p>
-              <p className="w-[14.5rem]">{item.amount}</p>
-              <p className="w-[14.5rem]">{item.product}</p>
-            </div>
-          );
-        })}
-      </div>
+    <main className="min-h-[calc(100vh-8rem)]">
+      <section className="flex justify-center gap-5">
+        <div>
+          <h2 className="text-center text-[1.5rem] font-bold pt-5">Order Details</h2>
+          <div className="flex p-2 w-[27rem] mx-auto">
+            <p className="w-[10rem]">Order Created At: </p>
+            <p className="px-2">: {formatDate(order?.createdAt)}</p>
+          </div>
+          <div className="flex p-2 w-[27rem] mx-auto">
+            <p className="w-[10rem]">Order Updated At </p>
+            <p className="px-2">: {formatDate(order?.updatedAt)}</p>
+          </div>
+          <div className="flex p-2 w-[27rem] mx-auto">
+            <p className="w-[10rem]">Order Id </p>
+            <p className="px-2">: {order?._id}</p>
+          </div>
+          <div className="flex p-2 w-[27rem] mx-auto">
+            <p className="w-[10rem]">Order Total </p>
+            <p className="px-2">: {formatPrice(order?.total)}</p>
+          </div>
+          <div className="flex p-2 w-[27rem] mx-auto">
+            <p className="w-[10rem]">Address </p>
+            <p className="px-2">: {order?.address}</p>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-center text-[1.5rem] font-bold  pt-5">Order Items</h2>
+          <div className="flex flex-col gap-3 ">
+            {order?.orderItems?.map((item, index) => {
+              return (
+                <div key={index}>
+                  <div className="w-[20rem] flex gap-2">
+                    <p className="w-[6rem]">Name</p>
+                    <p>: {item.name}</p>
+                  </div>
+                  <div className="w-[20rem] flex gap-2">
+                    <p className="w-[6rem]">Price</p>
+                    <p>: {formatPrice(item.price)}</p>
+                  </div>
+                  <div className="w-[20rem] flex gap-2">
+                    <p className="w-[6rem]">Amount</p>
+                    <p>: {item.amount}</p>
+                  </div>
+                  <div className="w-[20rem] flex gap-2">
+                    <p className="w-[6rem]">ProductId</p>
+                    <p>: {item.product}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col gap-3 justify-center items-center min-h-[screen-10rem]"
+        className="flex flex-col gap-3 justify-center items-center"
         autoComplete="off"
       >
-        <section className="flex gap-12 ">
+        <div className="flex gap-12 ">
           <div className="flex flex-col gap-3">
-            <label htmlFor="status" className="text-[#EEEDE8]">
+            {/* <label htmlFor="status" className="text-[#EEEDE8] text-center">
               Status
-            </label>
+            </label> */}
             <select
               name="status"
               id="status"
               value={orderInfo?.status}
               onChange={handleChange}
-              className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-full appearance-none placeholder-[#3A3B3C]"
+              className="bg-[#D9C6A7] text-[#0D1732] w-36 focus:outline-none border border-gray-300 rounded py-2 px-4 block w-full appearance-none placeholder-[#3A3B3C] mt-5"
             >
               {statusList?.map((item, index) => {
                 return (
@@ -102,14 +115,14 @@ const EditOrder = () => {
               })}
             </select>
           </div>
-        </section>
-        <div className="w-[20rem]">
-        <button
-          type="submit"
-          className={`btn bg-[#F8B664] hover:bg-[#C6A779] text-[#0D1732] w-full`}
-        >
-          Edit Order
-        </button>
+        </div>
+        <div className="w-[20rem] pb-5">
+          <button
+            type="submit"
+            className={`btn bg-[#F8B664] hover:bg-[#C6A779] text-[#0D1732] w-full`}
+          >
+            Edit Order
+          </button>
         </div>
       </form>
     </main>
