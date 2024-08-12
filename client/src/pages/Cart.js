80047a2abb6b4569d850596dda27447da3053261
Mehmet Ugur Satir <musatir@gmail.com>
2024-08-12 23:24:38 +0200

    style: Cart page style

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -19,16 +19,28 @@ const Cart = () => {
   const { currentUser } = useSelector((state) => state.auth);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [address, setAddress] = useState({street:"", number:"", postal:"", country:""});
-  const handleChange= (e)=>{
-    setAddress({...address, [e.target.name]:e.target.value})
-  }
+  const [address, setAddress] = useState({
+    street: "",
+    number: "",
+    postal: "",
+    country: "",
+  });
+  const handleChange = (e) => {
+    setAddress({ ...address, [e.target.name]: e.target.value });
+  };
 
-  const handleProceed = ()=>{
-    const orderAddress = address.street + " " +  String(address.number) + " " + String(address.postal) + " " + address.country;
+  const handleProceed = () => {
+    const orderAddress =
+      address.street +
+      " " +
+      String(address.number) +
+      " " +
+      String(address.postal) +
+      " " +
+      address.country;
     dispatch(addAddress(orderAddress));
-    navigate('/payment');
-  }
+    navigate("/payment");
+  };
   return (
     <main className="min-h-[calc(100vh-8rem)]">
       {/* This section lists the cart items */}
@@ -53,14 +65,14 @@ const Cart = () => {
               </div>
               <div className="flex justify-between w-[5rem] gap-2">
                 <p
-                  className="text-2xl text-center font-bold bg-slate-200 w-[2rem] rounded-lg "
+                  className="text-2xl text-center font-bold bg-slate-200 text-black w-[2rem] rounded-lg cursor-pointer"
                   onClick={() => dispatch(decreaseCartItem(item.productId))}
                 >
                   -
                 </p>
                 <p className="text-2xl font-bold">{item.amount}</p>
                 <p
-                  className="text-2xl text-center font-bold  bg-slate-200 w-[2rem] rounded-lg "
+                  className="text-2xl text-center font-bold  bg-slate-200 text-black w-[2rem] rounded-lg cursor-pointer"
                   onClick={() => dispatch(increaseCartItem(item.productId))}
                 >
                   +
@@ -68,7 +80,7 @@ const Cart = () => {
               </div>
               <div>
                 <MdDelete
-                  className="text-amber-700 text-3xl"
+                  className="text-amber-700 text-3xl cursor-pointer"
                   onClick={() => dispatch(removeCartItem(item.productId))}
                 />
               </div>
@@ -77,27 +89,67 @@ const Cart = () => {
         })}
       </section>
 
-      <section className="w-[20rem] bg-slate-200 mx-auto rounded-lg my-2">
-        <div>
-          <label htmlFor="street">Street</label>
-          <input type="text" id="street" name="street" value={address.street} onChange={handleChange}/>
+      <section className="w-[20rem] bg-slate-200 mx-auto rounded-lg my-2 p-4">
+        <div className="flex items-center mb-2">
+          <label htmlFor="street" className="text-[#0D1732] w-[5.5rem]">
+            Street
+          </label>
+          <input
+            type="text"
+            id="street"
+            name="street"
+            value={address.street}
+            onChange={handleChange}
+            placeholder="Street name..."
+            className="flex-1 bg-slate-200 text-[#0D1732] focus:outline-none border placeholder-slate-500 pl-2 py-1"
+          />
         </div>
-        <div>
-          <label htmlFor="number">Number</label>
-          <input type="number" id="number" name="number" value={address.number} onChange={handleChange}/>
+        <div className="flex items-center mb-2">
+          <label htmlFor="number" className="text-[#0D1732] w-[5.5rem]">
+            Number
+          </label>
+          <input
+            type="number"
+            id="number"
+            name="number"
+            value={address.number}
+            onChange={handleChange}
+            placeholder="Building number..."
+            className="flex-1 bg-slate-200 text-[#0D1732] focus:outline-none border placeholder-slate-500 pl-2 py-1"
+          />
         </div>
-        <div>
-          <label htmlFor="postal">Postal Code</label>
-          <input type="number" id="postal" name="postal" value={address.postal} onChange={handleChange}/>
+        <div className="flex items-center mb-2">
+          <label htmlFor="postal" className="text-[#0D1732] w-[5.5rem]">
+            Postal Code
+          </label>
+          <input
+            type="number"
+            id="postal"
+            name="postal"
+            value={address.postal}
+            onChange={handleChange}
+            placeholder="Postal code..."
+            className="flex-1 bg-slate-200 text-[#0D1732] focus:outline-none border placeholder-slate-500 pl-2 py-1"
+          />
         </div>
-        <div>
-          <label htmlFor="postal">Country</label>
-          <input type="text" id="country" name="country" value={address.country} onChange={handleChange}/>
+        <div className="flex items-center mb-2">
+          <label htmlFor="country" className="text-[#0D1732] w-[5.5rem]">
+            Country
+          </label>
+          <input
+            type="text"
+            id="country"
+            name="country"
+            value={address.country}
+            onChange={handleChange}
+            placeholder="Country..."
+            className="flex-1 bg-slate-200 text-[#0D1732] focus:outline-none border placeholder-slate-500 pl-2 py-1"
+          />
         </div>
       </section>
 
       {/* This section shows payment details */}
-      <section className="w-[20rem] bg-slate-200 mx-auto rounded-lg my-2">
+      <section className="w-[20rem] bg-slate-200 mx-auto rounded-lg my-2 text-[#0D1732]">
         <div className="flex justify-between border-b-2 border-slate-300 w-[80%] mx-auto pt-4">
           <h5>Cart Total </h5>
           <h5>{formatPrice(cartTotal)} </h5>
@@ -117,7 +169,10 @@ const Cart = () => {
       </section>
       <section className="flex justify-center">
         {currentUser ? (
-          <button className="bg-amber-700 rounded-lg text-white hover:bg-amber-600 w-[16rem] uppercase p-2 font-bold my-5" onClick={handleProceed}>
+          <button
+            className="bg-amber-700 rounded-lg text-white hover:bg-amber-600 w-[16rem] uppercase p-2 font-bold my-5"
+            onClick={handleProceed}
+          >
             Proceed to Checkout
           </button>
         ) : (
