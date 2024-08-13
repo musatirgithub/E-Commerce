60350dd7a0692865ae973cb704fd00ec4fc8d5d2
Mehmet Ugur Satir <musatir@gmail.com>
2024-08-13 23:01:20 +0200

    fix: Useless line remove

--- server/models/product.js
+++ server/models/product.js
@@ -74,7 +74,6 @@ ProductSchema.virtual('reviews', {
     localField:'_id',
     foreignField:'product',
     justOne:false,
-    // match:{rating:3},
     })
 
 ProductSchema.pre('remove', async function (next) {
