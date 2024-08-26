93b782d1f2f4123ddf8417ada071a9975973bb25
Mehmet Ugur Satir <musatir@gmail.com>
2024-08-26 23:32:37 +0200

    feat: Icon and functionality add

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
 import useAuthCalls from "../hooks/useAuthCalls";
 import { useSelector } from "react-redux";
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
+import { PiUserCirclePlusFill } from "react-icons/pi";
+import { FiLogIn } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
+import { BsFillKeyFill } from "react-icons/bs";
+import { MdManageAccounts } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import logo from "../pics/logo.jpg";
 
 const Navbar = () => {
   const { currentUser, role } = useSelector((state) => state.auth);
+  const { numItemsInCart } = useSelector((state) => state.order);
   const { logout } = useAuthCalls();
+  const navigate = useNavigate();
+
+  // this part of code works when a user has logged in
+
   if (currentUser) {
     return (
       <nav className="flex justify-between items-center h-[4rem] px-5  bg-[#D9C6A7] text-[#0D1732] text-[0.8rem] lg:text-[1.1rem] font-semibold">
+        {/* Logo and left side of the Navbar */}
         <div className="flex items-center gap-2 lg:gap-5">
           <div>
             <Link to="/">
@@ -17,18 +31,60 @@ const Navbar = () => {
           </div>
           <h4>{`Welcome ${currentUser}`}</h4>
         </div>
-        <div className="flex justify-evenly gap-3">
+
+        {/* Right side of the Navbar */}
+        <div className="flex justify-evenly gap-6">
+          <div
+            className="flex flex-col justify-center items-center cursor-pointer pt-1"
+            onClick={() => navigate("/")}
+          >
+            <FaHome size={"1.5rem"} />
+            <p className="text-[0.8rem]">Home</p>
+          </div>
           {role === "admin" ? (
-            <Link to="/Dashboard">Dashboard</Link>
+            <div
+              className="flex flex-col justify-center items-center cursor-pointer pt-1"
+              onClick={() => navigate("/Dashboard")}
+            >
+              <MdManageAccounts size={"1.5rem"} />
+              <p className="text-[0.8rem]">Dashboard</p>
+            </div>
           ) : null}
-          <Link to="/change-password">Change Password</Link>
-          <h4 onClick={logout} className="cursor-pointer">
-            Logout
-          </h4>
+          <div
+            className="flex flex-col justify-center items-center cursor-pointer pt-1"
+            onClick={() => navigate("/change-password")}
+          >
+            <BsFillKeyFill size={"1.5rem"} />
+            <p className="text-[0.8rem]">Change Password</p>
+          </div>
+          <div
+            className=" relative w-[2rem] cursor-pointer"
+            onClick={() => navigate("/cart")}
+          >
+            <div className="absolute top-0 right-0 w-[1.3rem] h-[1.3rem] bg-orange-600 rounded-full flex justify-center align-center">
+              <p className="text-[0.8rem] text-center  text-white font-semibold">
+                {numItemsInCart}
+              </p>
+            </div>
+            <div className="pt-1">
+              <PiShoppingCartSimpleBold size={"1.5rem"} />
+              <p className="text-[0.8rem]">Cart</p>
+            </div>
+          </div>
+          <div
+            onClick={logout}
+            className="flex flex-col justify-center items-center cursor-pointer pt-1"
+          >
+            <FiLogOut size={"1.5rem"} />
+            <h4 className="text-[0.8rem]">Logout</h4>
+          </div>
         </div>
       </nav>
     );
   }
+
+  // this part of code works when an unauthenticated person visits the site
+
   return (
     <nav className="flex justify-between items-center h-[4rem] px-5  bg-[#D9C6A7] text-[#0D1732] text-[0.8rem] lg:text-[1.1rem] font-semibold">
       <div className="flex items-center gap-2 lg:gap-5">
@@ -39,9 +95,26 @@ const Navbar = () => {
         </div>
         <h4>Welcome visitor</h4>
       </div>
-      <div className="flex justify-evenly gap-3">
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
+      <div className="flex justify-evenly gap-6">
+      <div
+            className="flex flex-col justify-center items-center cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            <FaHome size={"1.5rem"} />
+            <p className="text-[0.8rem]">Home</p>
+          </div>
+        <div
+          className="flex flex-col justify-center items-center cursor-pointer"
+          onClick={() => navigate("/register")}
+        >
+          <PiUserCirclePlusFill size={"1.5rem"} />
+          <p className="text-[0.8rem]">Register</p>
+        </div>
+        <div className="flex flex-col justify-center items-center cursor-pointer"
+          onClick={() => navigate("/login")}>
+          <FiLogIn size={"1.5rem"}/>
+          <p className="text-[0.8rem]">Login</p>
+        </div>
       </div>
     </nav>
   );
