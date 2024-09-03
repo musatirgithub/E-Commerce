6de269f6415a5d04f47a81e50de12112e0d7f5ed
Mehmet Ugur Satir <musatir@gmail.com>
2024-09-03 23:37:00 +0200

    fix: Optional chaining add

--- client/src/pages/SingleProductReviews.js
+++ client/src/pages/SingleProductReviews.js
@@ -5,11 +5,11 @@ const SingleProductReviews = () => {
   const {product} = useSelector((state)=>state.product);
   return (
     <main className="min-h-[calc(100vh-8rem)]">
-      <p className="text-3xl font-bold capitalize text-center py-3">{product.name}</p>
+      <p className="text-3xl font-bold capitalize text-center py-3">{product?.name}</p>
             <div className="w-[10rem] object-cover object-center mx-auto">
                 <img src={product?.image} alt={product?.name}/>
             </div>
-            {reviews.map((review)=>{
+            {reviews?.map((review)=>{
               return<div className="mx-auto w-[32rem] p-2" key={review._id}>
                 <div className=" flex justify-between">
                 <p className="text-[#1E4CB7] font-bold">{review.user.name}</p>
